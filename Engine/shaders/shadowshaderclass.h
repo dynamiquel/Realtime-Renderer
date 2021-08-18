c36480eca51effeda5164f10e9ee0f9821aa3269
dynamiquel <50085636+dynamiquel@users.noreply.github.com>
2021-08-18 16:14:50 +0100

    Add files via upload

---
+++ Engine/shaders/shadowshaderclass.h
@@ -0,0 +1,75 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: shadowshaderclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _SHADOWSHADERCLASS_H_
+#define _SHADOWSHADERCLASS_H_
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <d3d11.h>
+#include <DirectXMath.h>
+#include <fstream>
+class Light;
+using namespace std;
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: ShadowShaderClass
+////////////////////////////////////////////////////////////////////////////////
+class ShadowShaderClass
+{
+private:
+	struct MatrixBufferType
+	{
+		DirectX::XMMATRIX world;
+		DirectX::XMMATRIX view;
+		DirectX::XMMATRIX projection;
+		DirectX::XMMATRIX lightView;
+		DirectX::XMMATRIX lightProjection;
+	};
+
+	struct LightBufferType
+	{
+		DirectX::XMFLOAT4 ambientColor;
+		DirectX::XMFLOAT4 diffuseColor;
+	};
+
+	struct LightBufferType2
+	{
+		DirectX::XMFLOAT3 lightDirection;
+		float padding;
+	};
+
+public:
+	ShadowShaderClass();
+	ShadowShaderClass(const ShadowShaderClass&);
+	~ShadowShaderClass();
+
+	bool Initialize(ID3D11Device*, HWND);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext*, int, const DirectX::XMMATRIX&, const DirectX::XMMATRIX&, const DirectX::XMMATRIX&, ID3D11ShaderResourceView*, 
+				ID3D11ShaderResourceView*, Light*);
+
+private:
+	bool InitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
+	void ShutdownShader();
+	void OutputShaderErrorMessage(ID3D10Blob*, HWND, WCHAR*);
+
+	bool SetShaderParameters(ID3D11DeviceContext*, DirectX::XMMATRIX, DirectX::XMMATRIX, DirectX::XMMATRIX, ID3D11ShaderResourceView*, 
+							 ID3D11ShaderResourceView*, Light*);
+	void RenderShader(ID3D11DeviceContext*, int);
+
+private:
+	ID3D11VertexShader* m_vertexShader;
+	ID3D11PixelShader* m_pixelShader;
+	ID3D11InputLayout* m_layout;
+	ID3D11SamplerState* m_sampleStateWrap;
+	ID3D11SamplerState* m_sampleStateClamp;
+	ID3D11Buffer* m_matrixBuffer;
+	ID3D11Buffer* m_lightBuffer;
+	ID3D11Buffer* m_lightBuffer2;
+};
+
+#endif
